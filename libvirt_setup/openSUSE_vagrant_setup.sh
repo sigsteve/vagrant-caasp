fc962590e8095b4c672b88af8eaf53d8ff84fc18
Alessandro Renna <alessandro.renna@suse.com>
2020-03-02 18:51:32 +0100

    Update openSUSE_vagrant_setup.sh

--- libvirt_setup/openSUSE_vagrant_setup.sh
+++ libvirt_setup/openSUSE_vagrant_setup.sh
@@ -1,6 +1,7 @@
 # Taken from: https://github.com/SUSE/sesdev#install-vagrant
 
 $ sudo zypper ar https://download.opensuse.org/repositories/Virtualization:/vagrant/<repo> vagrant_repo
+$ sudo zypper ar https://download.opensuse.org/repositories/Virtualization:/containers/<repo> containers_repo
 $ sudo zypper ref
 $ sudo zypper -n install vagrant vagrant-libvirt
 Where <repo> can be openSUSE_Leap_15.1 or openSUSE_Tumbleweed
